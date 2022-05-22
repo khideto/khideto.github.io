07870f6756b0c0b35a39278946da751a7649919c
hidetoooon <xiurenjinteng895@gmail.com>
2022-05-22 20:40:14 +0900

    未入力の項目のラベルに色付けする処理を追加

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -5,47 +5,46 @@ function calculationCalorie(ageValue,heightValue,weightValue){
     let calorie = 0;
 
     if (sexValue == "man"){
-        calorie = (0.0481 * weightValue + 0.0234 * heightValue - 0.0138 * ageValue - 0.4235) * 1000 / 4.186;
+        calorie = Math.ceil((0.0481 * weightValue + 0.0234 * heightValue - 0.0138 * ageValue - 0.4235) * 1000 / 4.186);
     }else{
-        calorie = (0.0481 * weightValue + 0.0234 * heightValue - 0.0138 * ageValue - 0.9708) * 1000 / 4.186;
+        calorie = Math.ceil((0.0481 * weightValue + 0.0234 * heightValue - 0.0138 * ageValue - 0.9708) * 1000 / 4.186);
     }
     alert(`あなたの基礎代謝は${calorie}kcalです。`);
 }
 
 
 //入力値が空じゃないか判断する関数
-function inputCheck(inputvalue,inputname){
+function inputCheck(inputvalue,inputname,inputlabel){
     if(inputvalue == ("")){
-        return [1 , inputname];
+        return [1 , inputname,inputlabel];
     }else{
-        return [0 , inputname];
+        return [0 , inputname,inputlabel];
     }
 }
 
 //基礎代謝計算関数
 function baseCalorieCalculation(){
-    const inputAge = [document.querySelector("#age").value,"年齢"];
-    const inputHeight = [document.querySelector("#height").value,"身長"];
-    const inputWeight = [document.querySelector("#weight").value,"体重"];
+    const inputAge = [document.querySelector("#age").value,"年齢", "age"];
+    const inputHeight = [document.querySelector("#height").value,"身長","height"];
+    const inputWeight = [document.querySelector("#weight").value,"体重","weight"];
     let resultSum = 0;
     let alertText = "";
     let result = {};
 
-    result[0] = inputCheck(inputAge[0],inputAge[1]);
-    result[1] = inputCheck(inputHeight[0],inputHeight[1]);
-    result[2] = inputCheck(inputWeight[0],inputWeight[1]);
+    result[0] = inputCheck(inputAge[0],inputAge[1],inputAge[2]);
+    result[1] = inputCheck(inputHeight[0],inputHeight[1],inputHeight[2]);
+    result[2] = inputCheck(inputWeight[0],inputWeight[1],inputWeight[2]);
 
     for (let index = 0; index < 3; index++) {
         resultSum = resultSum + result[index][0];
-    }
-
-    if (resultSum > 0){
-        for (let index = 0; index < 3; index++) {
-            if (result[index][0]){
-                alertText = alertText + result[index][1] + ",";
-            }else{
-            }
+        if (result[index][0]){
+            alertText = alertText + result[index][1] + ",";
+            document.querySelector(`label[for = ${result[index][2]}]`).style.color = "Red";
+        }else{
+            document.querySelector(`label[for = ${result[index][2]}]`).style.color = "Black";
         }
+    }
+    if (resultSum > 0){
         alert(`${alertText}が未入力です。値を入力してから実行してください。`);
     }else{
         const returnAge = ageCalculation(inputAge[0]);
